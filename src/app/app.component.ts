24fda9439119c98880dd0c8a9f1520f294ecb8e0
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-06 09:47:34 +0100

    Add MyExperience component and integrate it into the app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MySkillsComponent } from './components/my-skills/my-skills.component';
+import { MyExperienceComponent } from './components/my-experience/my-experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, AboutMeComponent, MySkillsComponent],
+  imports: [
+    HeaderComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    MyExperienceComponent,
+    MyExperienceComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
---
+++ src/app/components/my-experience/my-experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-experience',
+  imports: [],
+  templateUrl: './my-experience.component.html',
+  styleUrl: './my-experience.component.less'
+})
+export class MyExperienceComponent {
+
+}

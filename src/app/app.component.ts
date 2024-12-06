e221c46f2f1e21d654e3fb8066930c0bc7de8975
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-06 08:45:00 +0100

    Add MySkills component to showcase skills with icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from './components/about-me/about-me.component';
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from './components/header/header.component';
+import { MySkillsComponent } from './components/my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent, AboutMeComponent],
+  imports: [HeaderComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'freelance-portfolio';

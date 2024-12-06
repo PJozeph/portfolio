05ef3b71ee09d7c10bdd6e3302b54182f705b037
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-06 11:03:05 +0100

    Add AboutMeMore and Projects components; update app layout and styles for improved structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MySkillsComponent } from './components/my-skills/my-skills.component';
 import { MyExperienceComponent } from './components/my-experience/my-experience.component';
+import { AboutMeMoreComponent } from './components/about-me-more/about-me-more.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,9 @@ import { MyExperienceComponent } from './components/my-experience/my-experience.
     AboutMeComponent,
     MySkillsComponent,
     MyExperienceComponent,
-    MyExperienceComponent
+    MyExperienceComponent,
+    AboutMeMoreComponent,
+    ProjectsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
---
+++ src/app/components/about-me-more/about-me-more.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me-more',
+  imports: [],
+  templateUrl: './about-me-more.component.html',
+  styleUrl: './about-me-more.component.less'
+})
+export class AboutMeMoreComponent {
+
+}

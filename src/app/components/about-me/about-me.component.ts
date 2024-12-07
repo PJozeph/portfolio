ee542ab25c6b269310bd4181734aace423124cfb
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-07 09:24:42 +0100

    Add server-side rendering support with Angular and Express; configure server routes and application settings

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me',
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.less'
+  styleUrl: './about-me.component.less',
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+  private meta: Meta = inject(Meta);
+  private title: Title = inject(Title);
 
+  ngOnInit(): void {}
 }

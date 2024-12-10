bd872c6b7e36844670ab9b66c4fa2ab6281795fa
Pallagi Joe <pallagijoe@gmail.com>
2024-12-10 18:22:19 +0100

    Refactor app.component.ts imports for improved organization and consistency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MySkillsComponent } from './components/my-skills/my-skills.component';
-import { MyExperienceComponent } from './components/my-experience/my-experience.component';
 import { AboutMeMoreComponent } from './components/about-me-more/about-me-more.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MyExperienceComponent } from './components/my-experience/my-experience.component';
+import { MySkillsComponent } from './components/my-skills/my-skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { TestimonialComponent } from './components/testimonial/testimonial.component';
-import { ContactComponent } from "./components/contact/contact.component";
-import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    AboutMeComponent,
-    MySkillsComponent,
-    MyExperienceComponent,
-    MyExperienceComponent,
-    AboutMeMoreComponent,
-    ProjectsComponent,
-    TestimonialComponent,
-    ContactComponent,
-    FooterComponent,
-    FooterComponent
-],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less',
+    selector: 'app-root',
+    imports: [
+        HeaderComponent,
+        AboutMeComponent,
+        MySkillsComponent,
+        MyExperienceComponent,
+        MyExperienceComponent,
+        AboutMeMoreComponent,
+        ProjectsComponent,
+        TestimonialComponent,
+        ContactComponent,
+        FooterComponent,
+        FooterComponent,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less',
 })
 export class AppComponent {
-  title = 'freelance-portfolio';
+    title = 'freelance-portfolio';
 }

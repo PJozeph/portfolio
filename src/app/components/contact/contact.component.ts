eee335807cd6bf55a2f5dafe800aa0571f524b45
Pallagi Joe <pallagijoe@gmail.com>
2024-12-10 18:09:09 +0100

    Add Angular Material theme and spinner; update contact form layout and styles

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,26 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
-  selector: 'app-contact',
-  imports: [ReactiveFormsModule],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.less',
+    selector: 'app-contact',
+    imports: [ReactiveFormsModule, MatProgressSpinnerModule],
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.less',
 })
 export class ContactComponent implements OnInit {
-  formBuilder = inject(FormBuilder);
-  contactFormGroup: FormGroup;
+    formBuilder = inject(FormBuilder);
+    contactFormGroup: FormGroup;
 
-  ngOnInit(): void {
-    this.contactFormGroup = this.formBuilder.nonNullable.group({
-      name: new FormControl<string>(''),
-      email: new FormControl<string>(''),
-      website: new FormControl<string>(''),
-      message: new FormControl<string>(''),
-    });
+    ngOnInit(): void {
+        this.contactFormGroup = this.formBuilder.nonNullable.group({
+            name: new FormControl<string>(''),
+            email: new FormControl<string>(''),
+            website: new FormControl<string>(''),
+            message: new FormControl<string>(''),
+        });
 
-    this.contactFormGroup.valueChanges.subscribe((value) => {
-      console.log(value);
-    });
-  }
+        this.contactFormGroup.valueChanges.subscribe(value => {
+            console.log(value);
+        });
+    }
 }

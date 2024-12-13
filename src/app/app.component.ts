b18bafcb7017def9709bb3e658f6f438f3f12dab
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-13 08:47:34 +0100

    Enhance navigation and component structure; add fragment scrolling functionality and update header links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { AboutMeMoreComponent } from './components/about-me-more/about-me-more.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -8,25 +8,43 @@ import { MyExperienceComponent } from './components/my-experience/my-experience.
 import { MySkillsComponent } from './components/my-skills/my-skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        HeaderComponent,
-        AboutMeComponent,
-        MySkillsComponent,
-        MyExperienceComponent,
-        MyExperienceComponent,
-        AboutMeMoreComponent,
-        ProjectsComponent,
-        TestimonialComponent,
-        ContactComponent,
-        FooterComponent,
-        FooterComponent,
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.less',
+  selector: 'app-root',
+  imports: [
+    HeaderComponent,
+    AboutMeComponent,
+    MySkillsComponent,
+    MyExperienceComponent,
+    MyExperienceComponent,
+    AboutMeMoreComponent,
+    ProjectsComponent,
+    TestimonialComponent,
+    ContactComponent,
+    FooterComponent,
+    FooterComponent,
+    RouterModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less',
 })
-export class AppComponent {
-    title = 'freelance-portfolio';
+export class AppComponent implements OnInit {
+  activatedRouter = inject(ActivatedRoute);
+  destoryRef: DestroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.activatedRouter.fragment
+      .pipe(filter(Boolean), takeUntilDestroyed(this.destoryRef))
+      .subscribe((fragment) => {
+        this.scrollToElement(fragment);
+      });
+  }
+
+  scrollToElement(element: string): void {
+    const el = document.getElementById(element);
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
